ae86b4ac36afbb96baa8b1ed494d1f478de80b1e
Andrés Gutiérrez <andres@andresgutgon.com>
2014-03-08 14:33:18 +0100

    Params ecommerce ID

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -5,45 +5,51 @@ require 'faraday'
 class OpinionsController < ApplicationController
 
   before_filter :load_data
+
   def index
 
-    # conn = Faraday.new
-    # response = conn.get 'http://localhost:3000/fake-api'
-    # response.status
-    # # binding.pry
-    # @api_response = response.body
+    if params[:ecommerce_id]
+      @ecommerce_id = params[:ecommerce_id]
+      # conn = Faraday.new
+      # response = conn.get 'http://localhost:3000/fake-api'
+      # response.status
+      # @opinions = response.body
+    end
+    # binding.pry
   end
 
   def fake_api
-    render json: @api_response
+    render json: @opinions
   end
 
   private
 
+  def get_ecommerce_id
+    @ecommerce_id = params[:ecommerce_id] ? params[:ecommerce_id] : 1
+  end
+
   def load_data
-    @api_response = {
-      :opinions => [
-        {
-          :opinion_id => 1,
-          :description => "Foooooo",
-          :rating => 1,
-          :date => '2013-03-13',
-          :date_add => '2013-02-13',
-          :date_update => '2013-02-13',
-          :order_id => 2,
-          :lang => "es"
-        },
-        {
-          :opinion_id => 1,
-          :description => "Foooooo",
-          :rating => 1,
-          :date => '2013-03-13',
-          :date_add => '2013-02-13',
-          :date_update => '2013-02-13',
-          :order_id => 2,
-          :lang => "es"
-        }
-      ]
-    }
+    @opinions = [
+      {
+        :opinion_id => 1,
+        :description => "Foooooo",
+        :rating => 1,
+        :date => '2013-03-13',
+        :date_add => '2013-02-13',
+        :date_update => '2013-02-13',
+        :order_id => 2,
+        :lang => "es"
+      },
+      {
+        :opinion_id => 1,
+        :description => "Foooooo",
+        :rating => 1,
+        :date => '2013-03-13',
+        :date_add => '2013-02-13',
+        :date_update => '2013-02-13',
+        :order_id => 2,
+        :lang => "es"
+      }
+    ]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HgOpinions::Application.routes.draw do
   root :to => 'opinions#index'
+  get 'ecommerce/:ecommerce_id/opinions', :to => 'opinions#index'
   get 'fake-api', :to => 'opinions#fake_api'
 end
