313f2f811e8192a3f296a37a45a89f83667bed54
Andrés Gutiérrez <andres@andresgutgon.com>
2014-03-08 12:48:26 +0100

    Remove test view

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -11,28 +11,6 @@ class OpinionsController < ApplicationController
     response.status
     # binding.pry
     @api_response = response.body
+    @foo = "ddddddd"
   end
-
-  def test
-        url = URI.parse('https://api.github.com/repos/technoweenie/faraday')
-
-        response = Net::HTTP.start(url.host, use_ssl: true) do |http|
-          http.get url.request_uri, 'User-Agent' => 'MyLib v1.2'
-        end
-
-        @foo =  response
-
-        case response
-            when Net::HTTPRedirection
-              # repeat the request using response['Location']
-            when Net::HTTPSuccess
-              repo_info = JSON.parse response.body
-        end
-
-        json = JSON.parse(response.body)
-
-        @parsed_json = json
-
-    end
-
 end
