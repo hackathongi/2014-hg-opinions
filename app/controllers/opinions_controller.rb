ff54046e5e238127e8b24fed366168956f20f476
Andrés Gutiérrez <andres@andresgutgon.com>
2014-03-08 12:26:09 +0100

    TEst faraday

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,33 +1,38 @@
 require 'net/https'
 require 'json'
+require 'faraday'
 
 class OpinionsController < ApplicationController
-  
+
 
   def index
+    conn = Faraday.new
+    response = conn.get 'https://api.github.com/repos/technoweenie/faraday'
+    response.status
+    @api_response = response.body
 
   end
 
   def test
-		url = URI.parse('https://api.github.com/repos/technoweenie/faraday')
+        url = URI.parse('https://api.github.com/repos/technoweenie/faraday')
 
-		response = Net::HTTP.start(url.host, use_ssl: true) do |http|
-		  http.get url.request_uri, 'User-Agent' => 'MyLib v1.2'
-		end
+        response = Net::HTTP.start(url.host, use_ssl: true) do |http|
+          http.get url.request_uri, 'User-Agent' => 'MyLib v1.2'
+        end
 
-		@foo =  response
+        @foo =  response
 
-		case response
-			when Net::HTTPRedirection
-			  # repeat the request using response['Location']
-			when Net::HTTPSuccess
-			  repo_info = JSON.parse response.body
-		end
+        case response
+            when Net::HTTPRedirection
+              # repeat the request using response['Location']
+            when Net::HTTPSuccess
+              repo_info = JSON.parse response.body
+        end
 
-		json = JSON.parse(response.body)
+        json = JSON.parse(response.body)
 
-		@parsed_json = json
+        @parsed_json = json
 
-	end
+    end
 
 end
